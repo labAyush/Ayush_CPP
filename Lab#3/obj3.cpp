7659506a6456207ee0a3660b1271d3cf914ed53c
labAyush <try.ayush006@gmail.com>
2024-06-25 08:13:24 +0545

    new file added

--- Lab#3/obj3.cpp
+++ Lab#3/obj3.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 class sphere{
     float r;
-    
+    public:
+    void set(float a)
+    {
+        r=a;
+    }
+    float vol(){
+        return(4*3.14*r*r*r/3);
+    }
+};
+int main(){
+    sphere s;
+    s.set(2);
+    float v=s.vol();
+    cout<<"The volume of the sphere is:"<<v;
+    return 0;
 }
